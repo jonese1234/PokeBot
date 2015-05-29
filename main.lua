efdb7a77146f765d9fd6b461d7f56fa3447f6495
Kyle Coburn <kylecoburn@gmail.com>
2015-05-29 11:27:55 -0700

    2.1

--- main.lua
+++ main.lua
@@ -9,7 +9,7 @@ local PAINT_ON     = true -- Display contextual information while the bot runs
 
 -- START CODE (hard hats on)
 
-VERSION = "2.0.2"
+VERSION = "2.1"
 
 local Data = require "data.data"
 
